7ed2efb618ed3b911d4ac7df3054d00596ce9018
TheWarmMind <pimphatboup@gmail.com>
2025-12-19 19:30:32 +0700

    Update App.py

--- App.py
+++ App.py
@@ -8,12 +8,12 @@ app = Flask(__name__)
 line_bot_api = LineBotApi(os.getenv("CHANNEL_ACCESS_TOKEN"))
 handler = WebhookHandler(os.getenv("CHANNEL_SECRET"))
 
-@app.route("/callback", methods=[’POST‘])
+@app.route("/callback", methods=['POST'])
 def callback():
-    signature = request.headers[’X-Line-Signature‘]
+    signature = request.headers['X-Line-Signature']
     body = request.get_data(as_text=True)
     handler.handle(body, signature)
-    return ’OK‘
+    return 'OK'
 
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
